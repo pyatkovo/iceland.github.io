67650d8deadba34fdfc9187aadd0ca0565218edf
pyatk <pyatkov.olezha@gmail.com>
2024-07-07 13:41:13 +0300

    minicifation-js

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,18 @@ const gulp = require('gulp');
 const less = require('gulp-less');
 const cleanCSS = require('gulp-clean-css');
 const rename = require('gulp-rename');
+const uglify = require('gulp-uglify');
+const ignore = require('gulp-ignore');
 // Путь до исходных и скомпилированных файлов
 const paths = {
     styles: {
         src: 'styles/*.less', // путь до less файла
         srcMain: 'styles/style.less',
         dest: 'styles/'       // путь где будет min.css
+    },
+    scripts: {
+        src: 'scripts/*.js',  // путь до JS файлов
+        dest: 'minified-scripts/'  // путь где будут минифицированные JS файлы
     }
 };
 
@@ -19,7 +25,20 @@ function styles() {
         .pipe(rename({ suffix: '.min' }))
         .pipe(gulp.dest(paths.styles.dest));
 }
+
+// Минификация JS файлов
+function scripts() {
+    return gulp.src(paths.scripts.src)
+        .pipe(ignore.exclude('*.min.js')) // игнорировать файлы с окончанием .min.js
+        .pipe(uglify())
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(gulp.dest(paths.scripts.dest));
+}
+
+
 function watch() {
     gulp.watch(paths.styles.src, styles);
+    gulp.watch(paths.scripts.src, scripts);
 }
-exports.default = gulp.series(styles, watch);
+
+exports.default = gulp.series(gulp.parallel(styles, scripts), watch);
